da560ac4150924d1fe5ab93fbd5ccfb3c62bd658
zjxxs391 <71650899+zjxxs391@users.noreply.github.com>
2026-02-17 03:50:12 +0000

    Deploying to gh-pages from @ zjxxs391/nav@6f45f6de9211d0a9608a1a563a83f76a5ab82228 🚀

--- chunk-GH2NTYUD.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./chunk-TCNGC2YJ.js";import{Ab as e,sb as r}from"./chunk-K4O7IQM6.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
