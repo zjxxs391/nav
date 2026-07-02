d6cce64424df2667036b7af6b7677c37287071d2
zjxxs391 <71650899+zjxxs391@users.noreply.github.com>
2026-07-02 00:32:35 +0000

    Deploying to gh-pages from @ zjxxs391/nav@0b66e8fc2734c8fdd047f5d0b0bd428b3c47e58b 🚀

--- chunk-JYBSZUT3.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./chunk-6EHMK6AQ.js";import{Ab as e,sb as r}from"./chunk-MC5IDXOX.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
